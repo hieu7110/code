c75ccaf4f48f2980a57080332aee8574a8119996
hieu7110 <ng184971@gmail.com>
2024-09-19 14:08:19 +0700

    Create bt_31.cpp

---
+++ bt_31.cpp
@@ -0,0 +1,8 @@
+https://oj.vnoi.info/problem/hp_thpt_21_b
+https://oj.vnoi.info/problem/th_thpt_22_c
+https://oj.vnoi.info/problem/bedao_r17_b
+https://oj.vnoi.info/problem/bedao_r18_a
+https://oj.vnoi.info/problem/fcb001_maxsum
+https://oj.vnoi.info/problem/fcb029_prefix
+https://oj.vnoi.info/problem/fcb055_maxmul
+https://oj.vnoi.info/problem/fcb055_fiboxor
